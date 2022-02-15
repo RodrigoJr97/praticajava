f1ecf33ac835935f72f0380e35436386f682c3b6
RodrigoJr97 <rodrigojr1997@hotmail.com>
2022-02-15 14:38:15 -0300

    Descobrindo maior e menor nota

--- praticajava1/src/cursojava/introducaopoo/executavel/ArrayVetor.java
+++ praticajava1/src/cursojava/introducaopoo/executavel/ArrayVetor.java
@@ -6,7 +6,7 @@ import cursojava.introducaopoo.classes.Disciplina;
 public class ArrayVetor {
 	public static void main(String[] args) {
 
-		double[] notas = { 24.5, 20.9, 21.7, 23.8 };
+		double[] notas = { 24.5, 20.9, 31.7, 23.8 };
 		double[] notas2 = { 28.9, 31.9, 25.7, 33.8 };
 
 		Aluno aluno = new Aluno("Rodrigo", "0097", "JDev Treinamento");
@@ -27,16 +27,35 @@ public class ArrayVetor {
 		System.out.println("Matricula Aluno: " + aluno.getMatricula());
 
 		for (Disciplina disc : aluno.getDisciplinas()) {
+			double maiorNota = 0.0;
+			double menorNota = 0.0;
+
 			System.out.println("-----------------------------------------");
 			System.out.println("Disciplina: " + disc.getDisciplina());
 
-			for (double nota : disc.getNota()) {
-				System.out.println("Nota: " + nota);
+			for (int pos = 0; pos < disc.getNota().length; pos++) {
+				System.out.println("Nota " + (pos + 1) + ": " + disc.getNota()[pos]);
+
+				if (pos == 0) {
+					maiorNota = disc.getNota()[pos];
+					menorNota = disc.getNota()[pos];
+				} else {
+					if (disc.getNota()[pos] > maiorNota) {
+						maiorNota = disc.getNota()[pos];
+					}
+
+					if (disc.getNota()[pos] < menorNota) {
+						menorNota = disc.getNota()[pos];
+					}
+
+				}
+
 			}
 
+			System.out.println("Maior Nota: " + maiorNota);
+			System.out.println("Menor Nota: " + menorNota);
+
 		}
-		
-		
 
 	}
 }
