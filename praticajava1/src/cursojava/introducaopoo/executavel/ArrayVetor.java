064ae770cdd7e1adef3fc314afff296e4710af2d
RodrigoJr97 <rodrigojr1997@hotmail.com>
2022-02-17 20:07:47 -0300

    Mostrar aluno com sua lista de disciplinas e suas notas

--- praticajava1/src/cursojava/introducaopoo/executavel/ArrayVetor.java
+++ praticajava1/src/cursojava/introducaopoo/executavel/ArrayVetor.java
@@ -8,6 +8,8 @@ public class ArrayVetor {
 
 		double[] notas = { 24.5, 20.9, 31.7, 23.8 };
 		double[] notas2 = { 28.9, 31.9, 25.7, 33.8 };
+		double[] notas3 = { 23.7, 18.9, 34.5, 28.9 };
+		double[] notas4 = { 22.9, 30.2, 27.3, 33.0 };
 
 		Aluno aluno = new Aluno("Rodrigo", "0097", "JDev Treinamento");
 
@@ -21,40 +23,42 @@ public class ArrayVetor {
 
 		aluno.getDisciplinas().add(disciplina);
 		aluno.getDisciplinas().add(disciplina2);
+		
+		Aluno aluno2 = new Aluno("Karina", "0124", "Unipac");
+		
+		Disciplina disciplina3 = new Disciplina();
+		disciplina3.setDisciplina("Biologia");
+		disciplina3.setNota(notas3);
 
-		System.out.println("Nome Aluno: " + aluno.getNome());
-		System.out.println("Escola: " + aluno.getNomeEscola());
-		System.out.println("Matricula Aluno: " + aluno.getMatricula());
-
-		for (Disciplina disc : aluno.getDisciplinas()) {
-			double maiorNota = 0.0;
-			double menorNota = 0.0;
-
-			System.out.println("-----------------------------------------");
-			System.out.println("Disciplina: " + disc.getDisciplina());
-
-			for (int pos = 0; pos < disc.getNota().length; pos++) {
-				System.out.println("Nota " + (pos + 1) + ": " + disc.getNota()[pos]);
-
-				if (pos == 0) {
-					maiorNota = disc.getNota()[pos];
-					menorNota = disc.getNota()[pos];
-				} else {
-					if (disc.getNota()[pos] > maiorNota) {
-						maiorNota = disc.getNota()[pos];
-					}
-
-					if (disc.getNota()[pos] < menorNota) {
-						menorNota = disc.getNota()[pos];
-					}
-
+		Disciplina disciplina4 = new Disciplina();
+		disciplina4.setDisciplina("Fisica");
+		disciplina4.setNota(notas4);
+		
+		aluno2.getDisciplinas().add(disciplina3);
+		aluno2.getDisciplinas().add(disciplina4);
+		
+		//-----------------------------------------------
+		
+		Aluno[] arrayAlunos = new Aluno[2];
+		arrayAlunos[0] = aluno;
+		arrayAlunos[1] = aluno2;
+		
+		/*For para percorrer o array de alunos*/
+		for (int pos = 0; pos < arrayAlunos.length; pos++) {
+			System.out.println("----------------------------------");
+			System.out.println("Nome do Aluno(a): " + arrayAlunos[pos].getNome());
+			
+			/*For que recupera a lista de disciplinas do aluno*/
+			for (Disciplina disc : arrayAlunos[pos].getDisciplinas()) {
+				System.out.println("\nDisciplina: " + disc.getDisciplina());
+				
+				/*For para percorrer o array de notas de cada disciplina*/
+				for (int posnota = 0; posnota < disc.getNota().length; posnota++) {
+					System.out.println((posnota+1) + "º Nota: " + disc.getNota()[posnota]);
 				}
-
+				
 			}
-
-			System.out.println("Maior Nota: " + maiorNota);
-			System.out.println("Menor Nota: " + menorNota);
-
+			
 		}
 
 	}
